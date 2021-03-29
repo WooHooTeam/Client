f81996f6a21d2aad88f6fbba788be03c3c825e85
woohyuk <dngur6344@gmail.com>
2021-03-29 22:27:56 +0900

    Token print 문 삭제

--- lib/auth.dart
+++ lib/auth.dart
@@ -23,7 +23,6 @@ class Auth {
         body: json.encode(msg));
     if (response.statusCode == 200) {
       // 만약 서버가 OK 응답을 반환하면, JSON을 파싱합니다.
-      print(response.body);
       return response.body;//json.decode(response.body)["access_token"];
     } else {
       // 만약 응답이 OK가 아니면, 에러를 던집니다.
