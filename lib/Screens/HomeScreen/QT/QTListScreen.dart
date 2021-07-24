dc4f7a2e40bb479c167766ef5f5d41a860d3654f
woohyuk <dngur6344@gmail.com>
2021-07-24 22:59:15 +0900

    게시글 목록을 출력해주는 화면 추가

--- lib/Screens/HomeScreen/QT/QTListScreen.dart
+++ lib/Screens/HomeScreen/QT/QTListScreen.dart
@@ -1,46 +1,83 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:myapp/Screens/HomeScreen/model/ServerProp.dart';
+import 'package:http/http.dart' as http;
+import 'package:myapp/Properties.dart' as prop;
 
 class QTListScreen extends StatelessWidget {
 
-  List<String> items;
-
+  Future<List<Content>> items;
   QTListScreen() {
-    items = List<String>.generate(20, (index) {
-      return "Item - $index";
-    });
+    items=fetchContent();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("큐티 목록")),
-        body: ListView.builder(
-            padding: const EdgeInsets.all(8),
-            physics:BouncingScrollPhysics(),
-            itemCount: items.length ,
-            itemBuilder: (context,index){
-              String item = items[index];
-              return ListTile(
-                title: Text(item),
-                subtitle: Text("dd"),
-          );
-        })
+        appBar: AppBar(title: Text("게시글 목록")),
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){
+            //추가할 부분 생성
+          },
+          child:Icon(Icons.add),
+          backgroundColor: Colors.purple,
+        ),
+        body: FutureBuilder<List<Content>>(
+            future: items,
+            builder: (context,snapshot) {
+              if (snapshot.hasData) {
+                List<Content> posting = snapshot.data ?? [];
+                return ListView.builder(
+                    padding: const EdgeInsets.all(8),
+                    itemCount: posting.length,
+                    itemBuilder: (context, index) {
+                      return new ListTile(
+                        leading: Icon(Icons.wysiwyg),
+                        title: Text(posting[index].title),
+                        subtitle: Text(posting[index].writer),
+                        trailing: Icon(Icons.arrow_forward_ios),
+                      );
+                    }
+                );
+              }
+              else if (snapshot.hasError) {
+                return Text("${snapshot.error}");
+              }
+              else return CircularProgressIndicator();
+            }
+        )
     );
   }
-  /*List<String> people;
-  QTListScreen(){
-    people = List<String>.generate(10, (index){
-      return "Item-$index";
-    });
+}
+
+Future<List<Content>> fetchContent() async{
+  ServerProp serverProp=ServerProp();
+  final response = await http.get(Uri.parse('http://localhost:8078'+'/content/findAll'),headers: {'Authorization':prop.token});
+  if(response.statusCode==200){
+    return ContentImpl().fromJson(json.decode(utf8.decode(response.bodyBytes)));
   }
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      padding: const EdgeInsets.all(8),
-      itemCount: people.length + 1,
-      itemBuilder: (BuildContext context, int index) {
-        return ListTile(title: Text("Item-$index"),);
-      },
-    );
-  }*/
+  else{
+    throw Exception('Failed to load post');
+  }
+}
+
+class Content{
+  final int contentId;
+  final DateTime datetime;
+  final String title;
+  final String maintext;
+  final String writer;
+
+  Content({this.contentId,this.datetime,this.title,this.maintext,this.writer});
+}
+class ContentImpl{
+
+  List<Content> fromJson(List<dynamic> json){
+    List<Content> contentList = [];
+    for(int i=0;i<json.length;i++){
+      contentList.add(Content(contentId:json[i]['contentId'],datetime:DateTime.parse(json[i]['datetime'].toString().substring(0,10)),title: json[i]['title'],maintext: json[i]['maintext'],writer: json[i]['writer']));
+    }
+    return contentList;
+  }
 }
